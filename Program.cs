99365ac436ff80c8abbcdee13942e9edd67e5ccc
lneiz <yuntsevich@ymail.com>
2023-02-03 03:37:29 +0500

    инициировал репозиторий и решил задачу №2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 < num2) Console.WriteLine($"max = {num2}, min = {num1}");
+else Console.WriteLine($"max = {num1}, min = {num2}");
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
