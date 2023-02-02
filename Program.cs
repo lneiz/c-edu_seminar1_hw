76d854151912402f9be8848870b15adf04aeea64
lneiz <yuntsevich@ymail.com>
2023-02-03 04:03:51 +0500

    решил задачи №№4, 6, 8

--- Program.cs
+++ Program.cs
@@ -11,3 +11,45 @@ else Console.WriteLine($"max = {num1}, min = {num2}");
 */
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+int max = 0;
+int[] nums = new int[3];
+Console.WriteLine("Введите числа: ");
+
+for(int i = 0; i < nums.Length; i++)
+{
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for(int i = 0; i < nums.Length; i++)
+{
+    if(nums[i] > max) max = nums[i];
+}
+
+Console.WriteLine("Максимальное число: " + max);
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while(i <= num)
+{
+    Console.Write(i + " ");
+    i += 2;
+} 
+*/
